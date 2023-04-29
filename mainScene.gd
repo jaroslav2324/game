a0bdb3381ad5c98ecec301527d88e5cf875f29fe
jaroslav2324 <jaro_r@mail.ru>
2023-04-29 23:53:17 +0300

    123

--- mainScene.gd
+++ mainScene.gd
@@ -3,6 +3,10 @@ extends Node2D
 @export var max_anger_count = 3
 @export var current_anger_count= 0
 
+@export var adding_velocity = 30
+@export var max_velocity = 220
+@export var back_acceleration = 0.3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ambient.play()
@@ -11,4 +15,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	apply_back_acceleration()
+
+func increase_velocity():
+		var vel_x = $"background".linear_velocity.x + adding_velocity
+		if vel_x > max_velocity:
+			vel_x = max_velocity
+		$"background".linear_velocity = Vector2(vel_x, 0)
+
+# back acceleration
+func apply_back_acceleration():
+	if $"background".linear_velocity.x < 0:
+		$"background".linear_velocity.x = 0
+	elif $"background".linear_velocity.x > 0:
+		$"background".linear_velocity.x -= back_acceleration
+
+
+func _on__stroke_on_water():
+	increase_velocity()
